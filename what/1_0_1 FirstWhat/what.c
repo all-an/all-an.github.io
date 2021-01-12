c7fedc65b80cf42363c5469f469e70c0032d02c6
all-an <all.ancapistan@gmail.com>
2021-01-12 07:50:27 -0300

    learning

---
+++ what/1_0_1 FirstWhat/what.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("say a number\n ");
+
+    int number = 42;
+
+    int attempt;
+
+    printf("Say a number ");
+
+    scanf("%d", &attempt);
+    printf("your attempt is %d ", attempt);
+}
