f1488ea1bb4541f7e6eb3f8e62d324553d87fe60
all-an <all.ancapistan@gmail.com>
2021-01-24 13:06:13 -0300

    learning

---
+++ see/1_0_1/multiply.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	int x,y;
+
+	printf("Entre o primeiro numero: ");
+	scanf("%d", &x);
+	
+	printf("Entre o segundo numero: ");
+	scanf("%d", &y);
+	
+	printf("A multiplicacao e:%d \n", x * y); 
+}
+
+
---
+++ see/guess.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	printf("hi");
+	printf("\nhi");
+}
+
+  //Learning how to declare variables
+  
+
+
+
