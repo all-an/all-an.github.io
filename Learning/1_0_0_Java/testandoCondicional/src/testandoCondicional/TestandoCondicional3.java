fe444fa0b6ceabd39967445850fc6e339d7cff23
all-an <all.ancapistan@gmail.com>
2021-04-04 12:05:46 -0300

    learning

---
+++ Learning/1_0_0_Java/testandoCondicional/src/testandoCondicional/TestandoCondicional3.java
@@ -0,0 +1,13 @@
+package testandoCondicional;
+
+public class TestandoCondicional3 {
+	public static void main(String[] args) {
+		System.out.println("Maitei Yvóra");
+		int test = 2;
+		if(test >= 3) {
+			System.out.println("Rohayhu");
+		} else {
+			System.out.println("Peteĩ");
+		}
+	}
+}
--- Learning/1_0_0_Java/variaveis/src/variaveis/Variaveis.java
+++ Learning/1_0_0_Java/variaveis/src/variaveis/Variaveis.java
@@ -3,6 +3,22 @@ package variaveis;
 public class Variaveis {
 	public static void main(String[] args) {
 		System.out.println("ola novo teste");
+		float idade = 37;
+		System.out.println("A idade é " + idade);
+		
+		idade = 30 + 10;
+		
+		System.out.println(idade);
+		
+		idade = 983 * 7;
+		
+		System.out.println(idade);
+		
+		idade = 1 / 54;
+		
+		// System.out.println(idade);
+		
+		
 	}
 
 }
